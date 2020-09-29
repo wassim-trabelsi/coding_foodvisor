a2a9a835bd8bcc2d8b8ae43fe9afdc7215c74335
wassim <wassimpong@hotmail.fr>
2020-09-29 14:43:41 +0200

    Update_status : Done Tests : Done

--- database.py
+++ database.py
@@ -4,13 +4,11 @@
 
 class Node(object):
 
-    def __init__(self, id, childs=[]):
+    def __init__(self, id):
         self.id = id
         self.isleaf = True
-        self.childs = childs
-
-        if len(self.childs) > 0:
-            self.leaf = False
+        self.childs = []
+        self.images = []
 
     def add_child(self, child):
         self.childs.append(child)
@@ -20,6 +18,9 @@ class Node(object):
         for child in childs:
             self.add_child(child)
 
+    def add_image(self, image):
+        self.images.append(image)
+
 
 class Database(object):
 
@@ -28,7 +29,8 @@ class Database(object):
         self.root = Node(root_id)
         # Dict of registered nodes
         self.id2node = {root_id: self.root}
-        # Reset extract
+        # Dict of registered images and their corresponding labels
+        self.image2labels = {}
         self.extract = {}
 
     def add_nodes(self, nodes):
@@ -42,7 +44,6 @@ class Database(object):
 
     def add_node(self, node):
         child_id, father_id = node
-
         if child_id in self.id2node:
             print('Warning !! {} is already registered, cannot add this id : skipped '.format(child_id))
             return
@@ -59,27 +60,46 @@ class Database(object):
         father = self.id2node[father_id]
         # Add the child to his father
         father.add_child(child)
+        # Update extract
+        coverage_images = [image for sister in father.childs for image in sister.images]
+        granularity_images = father.images
+        self.update_extract(coverage_images, granularity_images)
+
+    def update_extract(self, coverage_images, granularity_images):
+        for image in granularity_images:
+            if self.extract[image] in ['valid', 'granularity_staged']:
+                self.extract[image] = 'granularity_staged'
+        for image in coverage_images:
+            if self.extract[image] in ['valid', 'granularity_staged', 'coverage_staged']:
+                self.extract[image] = 'coverage_staged'
 
     def add_extract(self, image2labels):
         """
         The add_extract method takes a dict as input where the keys are image names,
         and values are list of node IDs (string).
         """
+        # Save the image2labels
+        self.image2labels = image2labels
         # Reset extract
+        self.reset_extract()
+
+    def reset_extract(self):
         self.extract = {}
-        for image, labels in image2labels.items():
-            # Check if all labels exist
-            if all([label in self.id2node for label in labels]):
+        for image, labels in self.image2labels.items():
+            # Check if all labels exist, otherwise it's invalid
+            if all([label_id in self.id2node for label_id in labels]):
                 # Check if all labels are at the last level of granularity
-                if all([self.id2node[label].isleaf for label in labels]):
+                last_level_granularity = []
+                for label_id in labels:
+                    label_node = self.id2node[label_id]
+                    label_node.add_image(image)
+                    last_level_granularity.append(label_node.isleaf)
+                if all(last_level_granularity):
                     self.extract[image] = 'valid'
                 else:
                     self.extract[image] = 'granularity_staged'
             else:
                 self.extract[image] = 'invalid'
-
-    def update_extract(self):
-        pass
 
     def get_extract_status(self):
         return self.extract
--- test.py
+++ test.py
@@ -1,24 +1,83 @@
 from database import Database
+import json
 
-# Initial graph
-build = [("core", None), ("A", "core"), ("B", "core"), ("C", "core"), ("C1", "C")]
-# Extract
-extract = {"img001": ["A"], "img002": ["C1"]}
-# Graph edits
-edits = [("A1", "A"), ("A2", "A")]
-
-# Get status (this is only an example, test your code as you please as long as it works)
-status = {}
-if len(build) > 0:
-    # Build graph
-    db = Database(build[0][0])
-    if len(build) > 1:
-        db.add_nodes(build[1:])
+def test1():
+    # Initial graph
+    build = [("core", None), ("A", "core"), ("B", "core"), ("C", "core"), ("C1", "C")]
+    # Extract
+    extract = {"img001": ["A", "B"], "img002": ["A", "C1"], "img003": ["B", "E"]}
     # Graph edits
-    db.add_nodes(edits)
-    # Add extract
-    db.add_extract(extract)
-    # Update status
-    status = db.get_extract_status()
-print(status)
+    edits = [("A1", "A"), ("A2", "A"), ("C2", "C")]
 
+    # Get status (this is only an example, test your code as you please as long as it works)
+    status = {}
+    if len(build) > 0:
+        # Build graph
+        db = Database(build[0][0])
+        if len(build) > 1:
+            db.add_nodes(build[1:])
+        # Add extract
+        db.add_extract(extract)
+        # Graph edits
+        db.add_nodes(edits)
+        # Update status
+        status = db.get_extract_status()
+        assert status == {'img001': 'granularity_staged', 'img002': 'coverage_staged', 'img003': 'invalid'}
+
+
+def test2():
+    # Initial graph
+    build = [("core", None), ("A", "core"), ("B", "core"), ("C", "core"), ("C1", "C")]
+    # Extract
+    extract = {"img001": ["A"], "img002": ["C1"]}
+    # Graph edits
+    edits = [("A1", "A"), ("A2", "A")]
+
+    # Get status (this is only an example, test your code as you please as long as it works)
+    status = {}
+    if len(build) > 0:
+        # Build graph
+        db = Database(build[0][0])
+        if len(build) > 1:
+            db.add_nodes(build[1:])
+        # Add extract
+        db.add_extract(extract)
+        # Graph edits
+        db.add_nodes(edits)
+        # Update status
+        status = db.get_extract_status()
+    assert status == {"img001": "granularity_staged", "img002": "valid"}
+
+def test3():
+    # Initial graph
+    with open('graph_build.json') as f:
+        build = json.load(f)
+    # Extract
+    with open('img_extract.json') as f:
+        extract = json.load(f)
+    # Graph edits
+    with open('graph_edits.json') as f:
+        edits = json.load(f)
+    # Expected_status
+    with open('expected_status.json') as f:
+        expected_status = json.load(f)
+
+    # Get status (this is only an example, test your code as you please as long as it works)
+    status = {}
+    if len(build) > 0:
+        # Build graph
+        db = Database(build[0][0])
+        if len(build) > 1:
+            db.add_nodes(build[1:])
+        # Add extract
+        db.add_extract(extract)
+        # Graph edits
+        db.add_nodes(edits)
+        # Update status
+        status = db.get_extract_status()
+    assert status == expected_status
+
+if __name__=='__main__':
+    test1()
+    test2()
+    test3()
